23a23b62a2fbee8aeeea82d6c5eff5602f3432cd
azotova <azotova@gmail.com>
2014-10-31 16:40:54 -0700

    hashtable working up to retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,30 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(i) === undefined) {
+    this._storage.set(i, [[k,v]]);
+  } else {
+    var temp = this._storage.get(i);
+    temp.push([k,v]);
+    this._storage.set(i, temp);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var tempArr=this._storage.get(i);
+  if(Array.isArray(tempArr)){
+    for(var i = 0; i < tempArr.length; i++){
+      if(tempArr[i][0] === k){
+        return tempArr[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+    var i = getIndexBelowMaxForKey(k, this._limit);
 };
 
 
