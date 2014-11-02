10b04450d3eeccd82aecea21ef40696bc943a5ba
azotova <azotova@gmail.com>
2014-11-01 18:44:50 -0700

    working binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,94 +1,36 @@
-/*var makeBinarySearchTree = function(value){
-  this.nodes = {
-    lefty: undefined,
-    righty: undefined
-  };
-  this.value = value;
-  // this.left = this.nodes.lefty;
-  // this.right = this.nodes.righty;
-  // console.log("thisleft:",this.left);
-};
-
-makeBinarySearchTree.prototype.left = function () {
-  return this.nodes.lefty;
-};
-
-makeBinarySearchTree.prototype.right = function () {
-  return this.nodes.righty;
-};
-
-makeBinarySearchTree.prototype.insert = function(value) {
-  console.log("value:", this.value, value);
-  console.log("this", this);
-  var newTree = new makeBinarySearchTree(value);
-  if(this.value > value){
-    console.log("valueThis:",this);
-    if(this.nodes.lefty === undefined){
-      this.nodes.lefty = newTree;
-      console.log("newlefty: ", newTree);
-    } else {
-      this.nodes.lefty.insert(value);
-    }
-  }
-  if(this.value < value){
-        console.log("value<This:",this);
-
-    if(this.nodes.righty === undefined){
-      this.nodes.righty = newTree;
-    } else {
-      this.nodes.righty.insert(value);
-    }
-  }
-};
-*/
 var makeBinarySearchTree = function(value){
   this.left = undefined;
   this.right = undefined;
   this.value = value;
-  // this.left = this.nodes.lefty;
-  // this.right = this.nodes.righty;
-  // console.log("thisleft:",this.left);
+  this.allValues = [value];
 };
-
-/*makeBinarySearchTree.prototype.left = function () {
-  return this.nodes.lefty;
-};
-
-makeBinarySearchTree.prototype.right = function () {
-  return this.nodes.righty;
-};*/
 
 makeBinarySearchTree.prototype.insert = function(value) {
-  console.log("value:", this.value, value);
-  console.log("this", this);
-  var newTree = new makeBinarySearchTree(value);
+  this.allValues.push(value);
   if(this.value > value){
-    console.log("valueThis:",this);
-    if(this.left === undefined){
-      this.left = newTree;
-      console.log("newlefty: ", newTree);
-    } else {
-      this.left.insert(value);
-    }
+    this.left === undefined ? this.left = new makeBinarySearchTree(value) : this.left.insert(value);
   }
   if(this.value < value){
-        console.log("value<This:",this);
+    this.right === undefined ? this.right = new makeBinarySearchTree(value) : this.right.insert(value);
+   }
+};
 
-    if(this.right === undefined){
-      this.right = newTree;
-    } else {
-      this.right.insert(value);
-    }
+
+makeBinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value){
+    return true;
+  } else if(this.left && (value < this.value)){
+    return this.left.contains(value);
+  } else if(this.right && (value > this.value)){
+    return this.right.contains(value);
   }
+  return false;
 };
 
-
-makeBinarySearchTree.prototype.contains = function() {
-  return 1;
-};
-
-makeBinarySearchTree.prototype.depthFirstLog = function() {
-  return 1;
+makeBinarySearchTree.prototype.depthFirstLog = function(callback) {
+  _.each(this.allValues, function(el){
+    callback(el);
+  })
 };
 /*
  * Complexity: What is the time complexity of the above functions?
